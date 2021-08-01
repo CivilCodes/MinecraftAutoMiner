d6752d99038281422ee0b14cba185604bace8007
ExT <88258708+ExTTT@users.noreply.github.com>
2021-07-31 22:24:00 -0400

    Create minecraftautomine.py

---
+++ minecraftautomine.py
@@ -0,0 +1,31 @@
+import pydirectinput
+import time
+time.sleep(5)
+
+pydirectinput.mouseDown()
+pydirectinput.keyDown('w')
+pydirectinput.keyDown('shift')
+
+time.sleep(1800)
+pydirectinput.press('2')
+
+time.sleep(1800)
+pydirectinput.press('3')
+
+time.sleep(1800)
+pydirectinput.press('4')
+
+time.sleep(1800)
+pydirectinput.press('5')
+
+time.sleep(1800)
+pydirectinput.press('6')
+
+time.sleep(1800)
+pydirectinput.press('7')
+
+time.sleep(1800)
+pydirectinput.press('8')
+
+time.sleep(1800)
+pydirectinput.press('9')
